a10386e29f420836d75389e99ecaf905c572b9a8
tong <reangsak1234@hotmail.com>
2024-12-23 11:27:17 +0700

    feat: algorithm skill checkpoint answer

--- question-3.js
+++ question-3.js
@@ -1,20 +1,22 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
-
+Answer: O(n) เพราะ ข้อนี้ใช้ linear search คือจะทำการลูปหาข้อมูลที่ต้องการจนกว่าจะเจอหรือจนหมดชุดข้อมูล 
+ยิ่งถ้าข้อมูลที่ต้องการอยู่ลำดับท้ายของชุดข้อมูลก็จะใช้จำนวนรอบการลูปที่เยอะขึ้น
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
-
+Answer: O(log n) เพราะ ข้อนี้ใช้ binary search คือทุกครั้งที่ลูปเพื่อหาข้อมูลที่ต้องการขอบเขตการค้นหาก็จะลดลงครึ่งหนึ่ง
+เพราะชุดข้อมูลมีการเรียงลำดับตัวอักษรมาแล้ว ทำให้การใช้ binary search ประหยัดเวลามากขึ้น
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
-
-
+Answer: ถ้าในแง่ของเวลา binary search จะทำได้ดีกว่าใช้ความเร็วในการค้นหาดีกว่าแบบ linear search
+แต่ในแง่การใช้งานก็ต้องเลือกใช้ให้ถูก เช่น
+- ถ้าข้อมูลมีจำนวนน้อยๆ หรือ ไม่ได้มีการเรียงลำดับข้อมูลมาก่อน ก็สามารถใช้ linear search ได้
+- ถ้าข้อมูลมีจำนวนมาก หรือ มีการเรียงลำดับมาแล้ว การใช้ binary search ก็จะทำเวลาได้ดีกว่า linear search 
+ดังนั้นเราควรเลือกใช้ให้ถูกประเภทเพื่อประสิทธิภาพที่ดีที่สุดในการค้นหาข้อมูล
 */
